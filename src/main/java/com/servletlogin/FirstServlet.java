b2089d61d3359911fc39d235505291ba499cbe87
shindeanant <shindeanant1997@gmail.com>
2021-12-11 22:45:12 +0530

    Added:First Servlet file added.

---
+++ src/main/java/com/servletlogin/FirstServlet.java
@@ -0,0 +1,22 @@
+package com.servletlogin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Hello")
+public class FirstServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3> Hello World My First Servlet </h3>");
+		out.close();
+
+	}
+}
